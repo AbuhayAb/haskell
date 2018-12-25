370e54fcd58b1706a23272ecc586ebfbe74861b6
Abuhay Abune <abuhay.abune@cs.huji.ac.il>
2018-12-25 14:29:58 +0200

    sol q2 in tar 3

--- ex3/tar_3.hs
+++ ex3/tar_3.hs
@@ -2,9 +2,11 @@
 -- abuhay abune
 -- tar 3  
 
-import Control.Monad
+import Control.Monad 
+import Control.Monad (replicateM)
 import Control.Monad.Trans.Writer
 import Control.Monad.State
+import System.Random
 
 ---- q1: Hanoi towers, tracking the steps with the writer monad 
 type Peg  = String
@@ -23,6 +25,19 @@ hanoi n = mapM_ putStrLn $ execWriter $ hanoi_ n "a" "b" "c"
 --- q2: A function that adds a random number every "even" time it used 
 --and subtracts a random number every "odd" time it is used (state monad) 
 
+
+
+rollDie :: State StdGen Int
+rollDie = do generator <- get
+             let (value, newGenerator) = randomR (1, 100) generator
+             put newGenerator
+             return value
+
+nRollState :: Int -> State StdGen Int
+nRollState n = do rhs <- replicateM n rollDie
+                  let lhs = take n $ cycle [1,-1]
+                      ret = foldr (+) 0 [x*y | (x,y) <- zip rhs lhs]
+                  return ret
+
+
 --- q3: Parse a floating point number
-
-
